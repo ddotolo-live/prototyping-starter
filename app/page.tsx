bdfa92ab98c6c494e79405beb6b9560698e32ea6
ddotolo-live <dylan.dotolo@livekit.io>
2025-09-19 16:17:04 -0500

    updated color tokens

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-full w-full flex flex-col gap-4 items-center justify-center">
+    <div className="h-full w-full flex flex-col gap-4 items-center justify-center bg-bg0">
       <div className="flex flex-col gap-4">
         <h1 className="text-4xl">Livekit Prototyping Starter Kit</h1>
         <p className="text-fg-1">
           This is a starter kit to help you vibe code faster without having to
           access the monorepo.
         </p>
-        <Button variant="primary" size="sm">
+        <Button variant="primary" size="lg">
           View Github Repo
         </Button>
         <Label>Enter your name</Label>
